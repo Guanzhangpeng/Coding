69ead85c48e6056d528c386a7f15966b460f4a07
gzp <zswangzp@163.com>
2018-06-04 16:39:38 +0800

    Initial Commit

---
+++ Coding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coding
+//
+//  Created by 管章鹏 on 2018/6/4.
+//  Copyright © 2018年 管章鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
